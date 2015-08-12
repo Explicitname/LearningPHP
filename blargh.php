affe5e52b2481c74b5aa3b8ff0f63b9e20f7e842
Explicitname <explicit6911@gmail.com>
2015-08-12 15:12:01 -0400

    Basic PHP Starting Point

---
+++ blargh.php
@@ -0,0 +1,18 @@
+<html>
+<head><title>Blargh PHP</title></head>
+
+<body>
+<?php print "<p>Blargh! </p>"; 
+$firstName = $_POST['firstname'];
+$lastName = $_POST['lastname'];
+
+echo $firstName;
+echo $lastName;
+
+?>
+</body>
+
+
+
+
+</html>
